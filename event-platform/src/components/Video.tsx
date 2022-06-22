79a0c7ec9b828c2f3082558973453dfb2d73243b
Weverson Diazz <wevers.info@gmail.com>
2022-06-22 20:14:46 -0300

    criando a seção de video, mais a seção com botões

--- event-platform/src/components/Video.tsx
+++ event-platform/src/components/Video.tsx
@@ -1,7 +1,36 @@
+import { DiscordLogo, Lightning } from "phosphor-react";
+
 export function Video() {
     return (
         <div className="flex-1">
-            <h1 className="text-lg">Video</h1>
+            <div className="bg-black flex justify-center">
+                <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video bg-yellow-500"></div>
+            </div>
+
+            <div className="p-8 max-w-[1100px] mx-auto">
+                <div className="flex items-start gap-16">
+                    <div className="flex-1">
+                        <h1 className="text-2xl font-bold">
+                            Ignite React
+                        </h1>
+                        <p className="mt-4 text-gray-200">
+                            Um evento da rocketseat onde vamos desenvolver uma plataforma de vídeo usando o React e outras tecnologias que estão sendo mais usada no mercado.
+                        </p>
+                    </div>
+
+
+                    <div className="flex flex-col gap-4">
+                        <a href="#" className="p-4 text-sm bg-green-500 flex items-center justify-center rounded font-bold uppercase gap-2 hover:bg-green-700 transition-colors">
+                            <DiscordLogo size={24} />
+                            Comunidade do Discord
+                        </a>
+                        <a href="#" className="p-4 text-sm border-blue-500 text-blue-500 flex items-center justify-center rounded font-bold uppercase gap-2 hover:bg-blue-500 hover:text-gray-900 transition-colors">
+                            <Lightning size={24} />
+                            Acesse o desafio
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
