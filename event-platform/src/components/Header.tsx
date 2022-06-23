30b26b0ad13aeb75a29dc8da35791e35c3dec600
Weverson Diazz <wevers.info@gmail.com>
2022-06-23 17:41:22 -0300

    Pegando os dados de cada evento e exibindo em tela

--- event-platform/src/components/Header.tsx
+++ event-platform/src/components/Header.tsx
@@ -4,7 +4,6 @@ export function Header() {
     return (
         <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">            
             <Logo />
-            <h1>Hello World</h1>
         </header>
     )
 }
--- event-platform/src/components/Video.tsx
+++ event-platform/src/components/Video.tsx
@@ -1,15 +1,62 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
 import { CaretRight, DiscordLogo, FileArrowDown, Image, Lightning } from "phosphor-react";
+import { gql, useQuery } from "@apollo/client";
 
 import "@vime/core/themes/default.css";
 
-export function Video() {
+const GET_LESSON_BY_SLUG_QUERY = gql`
+    query GetLessonBySlug($slug: String) {
+        lesson(where: {slug: $slug}) {
+            title
+            videoId
+            description
+            teacher {
+            bio
+            avatarURL
+            name
+            }
+        }
+    }
+`;
+
+interface GetLessonBySlugResponse {
+    lesson: {
+        title: string;
+        videoId: string;
+        description: string;
+        teacher: {
+            bio: string;
+            avatarURL: string;
+            name: string;
+        }
+    }
+}
+
+interface VideoProps {
+    lessonSlug: string;
+}
+
+export function Video({ lessonSlug } : VideoProps) {
+    const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+        variables: {
+            slug: lessonSlug
+        }
+    })
+
+    if (!data) {
+        return (
+            <div className="flex-1">
+                <p>Carregando...</p>
+            </div>
+        )
+    }
+
     return (
         <div className="flex-1">
             <div className="bg-black flex justify-center">
                 <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
                     <Player>
-                        <Youtube videoId={"pDbcC-xSat4"} />
+                        <Youtube videoId={data.lesson.videoId} />
                         <DefaultUi />
                     </Player>
                 </div>
@@ -19,22 +66,22 @@ export function Video() {
                 <div className="flex items-start gap-16">
                     <div className="flex-1">
                         <h1 className="text-2xl font-bold">
-                            Ignite React
+                            {data.lesson.title}
                         </h1>
                         <p className="mt-4 text-gray-200 leading-relaxed">
-                            Um evento da rocketseat onde vamos desenvolver uma plataforma de vídeo usando o React e outras tecnologias que estão sendo mais usada no mercado.
+                            {data.lesson.description}
                         </p>
 
                         <div className="flex items-center gap-4 mt-6">
                             <img 
                                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                                src="https://github.com/wevdiaz.png" 
-                                alt="foto pessoal do perfil do github" 
+                                src={data.lesson.teacher.avatarURL} 
+                                alt={`foto pessoal do perfil do professor ${data.lesson.teacher.name}` }
                             />
 
                             <div className="leading-relaxed">
-                                <strong className="font-bold text-2xl block">Weverson Diaz</strong>
-                                <span className="text-gray-200 text-sm block">Desenvolvedor Frontend</span>
+                                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
                             </div>
                         </div>
                     </div>
