ce1fc5988b81db3ad53b9b99466436edab8b528c
Weverson Diazz <wevers.info@gmail.com>
2022-06-22 21:18:27 -0300

    add video na aplicação e estilização

--- event-platform/src/components/Video.tsx
+++ event-platform/src/components/Video.tsx
@@ -1,10 +1,18 @@
-import { DiscordLogo, Lightning } from "phosphor-react";
+import { DefaultUi, Player, Youtube } from "@vime/react";
+import { CaretRight, DiscordLogo, FileArrowDown, Image, Lightning } from "phosphor-react";
+
+import "@vime/core/themes/default.css";
 
 export function Video() {
     return (
         <div className="flex-1">
             <div className="bg-black flex justify-center">
-                <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video bg-yellow-500"></div>
+                <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+                    <Player>
+                        <Youtube videoId={"pDbcC-xSat4"} />
+                        <DefaultUi />
+                    </Player>
+                </div>
             </div>
 
             <div className="p-8 max-w-[1100px] mx-auto">
@@ -13,9 +21,22 @@ export function Video() {
                         <h1 className="text-2xl font-bold">
                             Ignite React
                         </h1>
-                        <p className="mt-4 text-gray-200">
+                        <p className="mt-4 text-gray-200 leading-relaxed">
                             Um evento da rocketseat onde vamos desenvolver uma plataforma de vídeo usando o React e outras tecnologias que estão sendo mais usada no mercado.
                         </p>
+
+                        <div className="flex items-center gap-4 mt-6">
+                            <img 
+                                className="h-16 w-16 rounded-full border-2 border-blue-500"
+                                src="https://github.com/wevdiaz.png" 
+                                alt="foto pessoal do perfil do github" 
+                            />
+
+                            <div className="leading-relaxed">
+                                <strong className="font-bold text-2xl block">Weverson Diaz</strong>
+                                <span className="text-gray-200 text-sm block">Desenvolvedor Frontend</span>
+                            </div>
+                        </div>
                     </div>
 
 
@@ -29,6 +50,37 @@ export function Video() {
                             Acesse o desafio
                         </a>
                     </div>
+                </div>
+
+                <div className="gap-8 mt-20 grid grid-cols-2">
+                    <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+                        <div className="bg-green-700 h-full p-6 flex items-center">
+                            <FileArrowDown size={40} />
+                        </div>
+                        <div className="py-6 leading-relaxed">
+                            <strong className="text-2xl">Material complementar</strong>
+                            <p className="text-sm text-gray-200 mt-2"> 
+                                Acesse o material complementar para acelerar o seu desenvolvimento e aumentar o aprendizado
+                            </p>
+                        </div>
+                        <div className="h-full p-6 flex items-center">
+                            <CaretRight size={24} />
+                        </div>
+                    </a>
+                    <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+                        <div className="bg-green-700 h-full p-6 flex items-center">
+                            <Image size={40} />
+                        </div>
+                        <div className="py-6 leading-relaxed">
+                            <strong className="text-2xl">Wallpaper exclusivos</strong>
+                            <p className="text-sm text-gray-200 mt-2"> 
+                                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
+                            </p>
+                        </div>
+                        <div className="h-full p-6 flex items-center">
+                            <CaretRight size={24} />
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
