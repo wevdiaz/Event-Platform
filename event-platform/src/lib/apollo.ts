6bea308ce91b37be6d992a9e9847cca4e237d218
Weverson Diazz <wevers.info@gmail.com>
2022-06-24 15:27:21 -0300

    configurando variavel de ambiente e para criação de dados

--- event-platform/src/lib/apollo.ts
+++ event-platform/src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: "https://api-sa-east-1.graphcms.com/v2/cl4od2w9a1xmx01xxeznbd7q0/master",
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        "Authorization": `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
